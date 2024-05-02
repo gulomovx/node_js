0ff0818a80c636318245f7d58bc727c1605d4b75
gulomovx <xgulomov447@gmail.com>
2024-05-02 11:22:53 +0500

    node js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(
-            `<h2>Send email</h2>
-            <form>
-            <input name='email' type='email' placeholder='enter email'/>
-            <button type='submit'>Send</button>
-            </form>
-            `
-        )
-    } else if (req.method === 'POST') {
-        res.end('Email added')
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "templates", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw new Error();
+          res.end(content);
+        }
+      );
+    } else if (req.url === '/about') {
+        fs.readFile(
+            path.join(__dirname, "templates", "about.html"),
+            "utf-8",
+            (err, content) => {
+              if (err) throw new Error();
+              res.end(content);
+            }
+          );
     }
-})
+  } else if (req.method === "POST") {
+    res.end("Email added:");
+  }
+});
 
-// server 3000 portda ekranga hello there  textini chiqarmoqda va end() bn res ni toxtatmoqda 
+// server 3000 portda ekranga hello there  textini chiqarmoqda va end() bn res ni toxtatmoqda
 server.listen(3001, () => {
-    console.log('server started');
-    console.log('salom');
-})
+  console.log("server started");
+  console.log("salom");
+});
 
 // server 3000 portda server started ni log qily....
